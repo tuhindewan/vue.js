d12a28d1e236ea23d226b0d8fb51125deaddb994
tuhin <tuhinsshadow@gmail.com>
2018-01-14 16:21:19 +0600

    data binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var app = new Vue({
 	el:'#vue-app',
 	data:{
 		name:'Tuhin',
-		job:'Techadea'
+		job:'Techadea',
+		website:'http://www.techadea.com',
+		websitetag:'<a href="http://www.techadea.com">Techadea Website Tag</a>'
 	},
 	methods:{
 		greet:function(time){
