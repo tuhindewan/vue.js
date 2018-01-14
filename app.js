3891cb179b12577bdb88a417c3fb0787e3289602
tuhin <tuhinsshadow@gmail.com>
2018-01-14 17:31:02 +0600

    events

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ var app = new Vue({
 		name:'Tuhin',
 		job:'Techadea',
 		website:'http://www.techadea.com',
-		websitetag:'<a href="http://www.techadea.com">Techadea Website Tag</a>'
+		websitetag:'<a href="http://www.techadea.com">Techadea Website Tag</a>',
+		age:'25'
 	},
 	methods:{
 		greet:function(time){
 			return "Good" +" " + time + " " +this.name;
+		},
+		add:function(inc){
+			this.age += inc;
+		},
+		substract:function(dec){
+			this.age -= dec;
 		}
 	}
 });
