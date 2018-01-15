668b724ac4c1607b1012db72be62cf911c353181
tuhin <tuhinsshadow@gmail.com>
2018-01-15 12:59:31 +0600

    computed properties

--- app.js
+++ app.js
@@ -1,32 +1,21 @@
 var app = new Vue({
 	el:'#vue-app',
 	data:{
-		name:'Tuhin',
-		job:'Techadea',
-		website:'http://www.techadea.com',
-		websitetag:'<a href="http://www.techadea.com">Techadea Website Tag</a>',
-		age:'25',
-		name2:'',
-		age2:''
+		age:20,
+		a:0,
+		b:0
 	},
 	methods:{
-		greet:function(time){
-			return "Good" +" " + time + " " +this.name;
+		
+	},
+	computed:{
+		addToA:function(){
+			console.log('addToA');
+			return this.a + this.age;
 		},
-		add: function(inc){
-            this.age += inc;
-        },
-        substract: function(dec){
-            this.age -= dec;
-        },
-		click:function(){
-			alert("This link will take you to the company website");
-		},
-		logName:function(){
-			console.log('You entered your name');
-		},
-		logAge:function(){
-			console.log('You entered your age');
+		addToB:function(){
+			console.log('addToB');
+			return this.b + this.age;
 		}
 	}
 });
